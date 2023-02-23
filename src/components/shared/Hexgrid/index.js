ee23212a836fe7bc12a58b015f313fd61138570e
anirudhsss <anirudh.s@ionixxtech.com>
2023-02-23 23:34:02 +0530

    minor

--- src/components/shared/Hexgrid/index.js
+++ src/components/shared/Hexgrid/index.js
@@ -50,7 +50,7 @@ export const Hexgrid = ({
 
     //console.log('matchedMonths', matchedMonths)
     return (
-        <Box sx={{ width: '1474px', height: '84vh', position: 'static', zIndex: 100, }}>
+        <Box sx={{ width: '1440px', height: '84vh', position: 'static', zIndex: 100, }}>
             <HexGrid width={'100%'} height={'100%'} viewBox={`${data?.viewboxMinX} ${data?.viewboxMinY} ${data?.viewboxWidth} ${data?.viewboxHeight}`}>
                 <Layout size={{ x: 13, y: 13 }} flat={false} spacing={1.1} origin={{ x: -53, y: -27 }}>
                     {matchedMonths?.map((item, index) => {
