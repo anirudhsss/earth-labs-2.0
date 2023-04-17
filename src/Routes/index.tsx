39ad8fd5071ded7dd1b2e86683b0045979e20340
nikpaiyudev <nikhilradhakrishnan.work@gmail.com>
2023-04-17 10:08:27 +0530

    Added new component infoField.tsx

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -2,22 +2,7 @@ import { createBrowserRouter, createRoutesFromElements, Route, Navigate } from "
 import { UserHomepage } from "components/UserHomepage";
 import { Wallet } from "components/Wallet";
 import { Discovery } from "components/Discovery";
-import App from "App";
-import { LandingPage } from "components/LandingPage";
 
-// declare function createRoutesFromElements(
-//     children: React.ReactNode
-// ): RouteObject[];
-
-// interface RouteObject {
-//     caseSensitive?: boolean;
-//     children?: RouteObject[];
-//     element?: React.ReactNode;
-//     index?: boolean;
-//     path?: string;
-// }
-
-// const UserHomepage = () => <div>Maps</div>
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -29,7 +14,6 @@ export const router = createBrowserRouter(
                     }}
                 />}
             />
-            {/* <Route path="/" element={<LandingPage />} /> */}
             <Route path="/maps" element={<UserHomepage />} />
             <Route path="/discovery" element={<Discovery />} />
             <Route path="/wallet" element={<Wallet />} />
